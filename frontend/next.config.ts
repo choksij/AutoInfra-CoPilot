8c2f20c30917d162c3042a90d5df15734028bdda
Jeet Choksi <jeet.choksi@colorado.edu>
2025-10-05 17:42:20 -0400

    frontend-2

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingRoot: __dirname, // ensure project root
+  experimental: {
+    typedRoutes: true,
+  },
 };
 
 export default nextConfig;
